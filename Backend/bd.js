ac8526daf29dcca693b2216c71f13fc554f2b600
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-02-27 23:26:37 +0530

    Repo initiated wiith backend and frontend

---
+++ Backend/bd.js
@@ -0,0 +1,34 @@
+const mongoonse = require( 'mongoose' );;
+
+mongoonse.connect('mongodb+srv://whaleInSpace:aHX18KLJaNmCnQrf@0x.bxbegwj.mongodb.net/Wallet-App');
+
+const userSchema =  mongoose.Schema({
+    userName : {
+        type: String,
+        required : true,
+        unique : true,
+        trim: true,
+        lowercase: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    password : {
+        type : String,
+        required: true,
+        minlength: 8
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim : true,
+        maxlength: 30
+    },
+    lastName:{
+        type: String,
+        required : true,
+        trim: true,
+        maxlength: 30
+    }
+});
+const User = mongoonse.model("Users", userSchema);
+
